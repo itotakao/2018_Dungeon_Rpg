36190ca3cc91768f5803727c213184b38d4dbb87
伊藤貴央 <kurokkingu@gmail.com>
2018-10-02 14:32:51 +0900

    new : 装備欄を管理する機能を作成

---
+++ Assets/_Application/Scripts/Objects/EquipmentColumn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class EquipmentColumn : MonoBehaviour 
+{
+    public Item item = null;
+
+    public bool IsLock { get { return lockIcon.activeSelf; }}
+
+    [Space(10)]
+
+    [SerializeField]
+    Image iconImage = null;
+    [SerializeField]
+    Text nameText = null;
+    [SerializeField]
+    Text informationText = null;
+    [SerializeField]
+    GameObject lockIcon = null;
+
+
+	void Start () {
+
+        if (!item) { return; }
+
+        iconImage.sprite = item.GetIcon();
+        nameText.text = item.GetItemName();
+        informationText.text = item.GetInformation();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
