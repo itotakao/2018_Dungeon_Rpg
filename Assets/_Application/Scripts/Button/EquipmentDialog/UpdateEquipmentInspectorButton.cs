3864647e2f74014f15614630c380dbb62a92c9ca
伊藤貴央 <kurokkingu@gmail.com>
2018-10-06 14:05:30 +0900

    chg : 装備欄を選択したい際にインスペクターが表示されるように

---
+++ Assets/_Application/Scripts/Button/EquipmentDialog/UpdateEquipmentInspectorButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(EquipmentColumn))]
+public class UpdateEquipmentInspectorButton : MonoBehaviour
+{
+    public EquipmentUI EquipmentUI { get { return EquipmentUI.Current; } }
+
+    EquipmentColumn equipmentColumnCache = null;
+    EquipmentColumn EquipmentColumn { get { return equipmentColumnCache ? equipmentColumnCache : GetComponent<EquipmentColumn>(); } }
+
+    public void OnButton()
+    {
+        EquipmentUI.UpdateInspectorUI(EquipmentColumn.item);
+    }
+}
