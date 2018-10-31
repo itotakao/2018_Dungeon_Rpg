fe52e670dd2767775be47a43aa7f6cd5e012cf65
伊藤貴央 <kurokkingu@gmail.com>
2018-10-31 21:15:44 +0900

    new : ステータスボタンでステータスを表示するように

--- Assets/_Application/Scripts/UI/Battle/StatusUI.cs
+++ Assets/_Application/Scripts/UI/Battle/StatusUI.cs
@@ -9,8 +9,21 @@ namespace Battle
     {
         public static StatusUI Current { get; private set; }
 
+        public PlayerManager PlayerManager { get { return PlayerManager.Current; } }
+
         [SerializeField]
         GameObject dialog;
+
+        [SerializeField]
+        Text healthText = null;
+        [SerializeField]
+        Text attackText = null;
+        [SerializeField]
+        Text defenceText = null;
+        [SerializeField]
+        Text speedText = null;
+        [SerializeField]
+        Text attackIntervalText = null;
 
         void Awake()
         {
@@ -25,6 +38,20 @@ namespace Battle
         public void ShowDialog(bool isShow)
         {
             dialog.SetActive(isShow);
+
+            if(isShow)
+            {
+                UpdateText();
+            }
+        }
+
+        void UpdateText()
+        {
+            healthText.text = "体力: " + PlayerManager.GetMaxHealth();
+            attackText.text = "攻撃: " + PlayerManager.GetAttack();
+            defenceText.text = "防御: " + PlayerManager.GetDefence();
+            speedText.text = "敏捷: " + PlayerManager.GetSpeed();
+            attackIntervalText.text = "攻撃間隔: " + PlayerManager.GetAttackInterval();
         }
     }
 }
